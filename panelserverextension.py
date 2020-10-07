8dc8bed3275051577ff8140d681c3fca5ee3b9a4
Pedot T <thomas.pedot@gmail.com>
2020-10-07 22:39:52 +0200

    Change dir for binder

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,6 @@
 from subprocess import Popen
-
+import os
 def load_jupyter_server_extension(nbapp):
     """serve the gapminder.ipynb directory with bokeh server"""
+    os.chdir("/notebooks")
     Popen(["panel", "serve", "ind_frag_num_communes-mybinder.ipynb", "--allow-websocket-origin=*"])
